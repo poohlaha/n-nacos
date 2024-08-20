3194ecd1ca7e1388db452ad39353599733ca9b22
poohlaha <smile.love.tao@gmil.com>
2024-08-20 17:30:14 +0800

    修改流水线

--- src-tauri/sql/nacos.sql
+++ src-tauri/sql/nacos.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80039 (8.0.39)
  File Encoding         : 65001
 
- Date: 12/08/2024 18:25:31
+ Date: 20/08/2024 17:27:37
 */
 
 SET NAMES utf8mb4;
@@ -26,8 +26,7 @@ CREATE TABLE `pipeline` (
   `server_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '服务器 ID',
   `tag_id` varchar(255) DEFAULT NULL,
   `last_run_time` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '最后运行时间',
-  `duration` varchar(255) DEFAULT NULL,
-  `stage_run_index` int DEFAULT NULL COMMENT '运行到哪一步',
+  `last_run_id` varchar(255) DEFAULT NULL COMMENT '最后运行ID',
   `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '当前运行状态',
   `create_time` varchar(255) DEFAULT NULL,
   `update_time` varchar(255) DEFAULT NULL,
@@ -76,16 +75,101 @@ CREATE TABLE `pipeline_process` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ----------------------------
+-- Table structure for pipeline_runtime
+-- ----------------------------
+DROP TABLE IF EXISTS `pipeline_runtime`;
+CREATE TABLE `pipeline_runtime` (
+  `id` varchar(255) NOT NULL,
+  `pipeline_id` varchar(255) DEFAULT NULL COMMENT '流水线ID',
+  `project_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '项目名称，根据 url 获取',
+  `order` int DEFAULT NULL COMMENT '顺序',
+  `tag` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '标签',
+  `basic` longtext,
+  `stages` longtext COMMENT 'stages',
+  `status` varchar(255) DEFAULT NULL COMMENT '运行状态',
+  `start_time` varchar(255) DEFAULT NULL COMMENT '开始时间',
+  `duration` varchar(255) DEFAULT NULL COMMENT '运行时长, 单位秒',
+  `stage_index` int DEFAULT NULL COMMENT 'stage 运行到哪一步, 从 1 开始计算',
+  `group_index` int DEFAULT NULL COMMENT 'group 运行到哪一步, 从 0 开始计算',
+  `step_index` int DEFAULT NULL COMMENT 'step 运行到哪一步, 从 0 开始计算',
+  `finished` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '是否完成',
+  `remark` varchar(255) DEFAULT NULL COMMENT '运行备注',
+  `log` varchar(500) DEFAULT NULL COMMENT '日志文件地址',
+  `create_time` varchar(255) DEFAULT NULL,
+  `update_time` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for pipeline_runtime_snapshot
+-- ----------------------------
+DROP TABLE IF EXISTS `pipeline_runtime_snapshot`;
+CREATE TABLE `pipeline_runtime_snapshot` (
+  `id` varchar(255) NOT NULL,
+  `runtime_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '运行记录ID',
+  `node` varchar(255) DEFAULT NULL COMMENT 'nodeJs 版本号',
+  `branch` varchar(255) DEFAULT NULL COMMENT '分支',
+  `make` varchar(255) DEFAULT NULL COMMENT 'Make 命令',
+  `command` varchar(255) DEFAULT NULL COMMENT '本机安装的命令',
+  `script` varchar(255) DEFAULT NULL COMMENT 'package.json 中的 scripts 命令',
+  `create_time` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `update_time` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for pipeline_runtime_variable
+-- ----------------------------
+DROP TABLE IF EXISTS `pipeline_runtime_variable`;
+CREATE TABLE `pipeline_runtime_variable` (
+  `id` varchar(255) NOT NULL,
+  `snapshot_id` varchar(255) DEFAULT NULL,
+  `order` int DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `value` varchar(255) DEFAULT NULL,
+  `description` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '描述',
+  `create_time` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `update_time` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
 -- Table structure for pipeline_stage
 -- ----------------------------
 DROP TABLE IF EXISTS `pipeline_stage`;
 CREATE TABLE `pipeline_stage` (
   `id` varchar(255) NOT NULL,
   `process_id` varchar(255) DEFAULT NULL,
+  `order` int DEFAULT NULL,
   `create_time` varchar(255) DEFAULT NULL,
   `update_time` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for pipeline_status
+-- ----------------------------
+DROP TABLE IF EXISTS `pipeline_status`;
+CREATE TABLE `pipeline_status` (
+  `id` varchar(255) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `value` varchar(255) DEFAULT NULL,
+  `create_time` varchar(255) DEFAULT NULL,
+  `update_time` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of pipeline_status
+-- ----------------------------
+BEGIN;
+INSERT INTO `pipeline_status` (`id`, `name`, `value`, `create_time`, `update_time`) VALUES ('1', 'No', 'No', '2024-08-13 15:13:01', NULL);
+INSERT INTO `pipeline_status` (`id`, `name`, `value`, `create_time`, `update_time`) VALUES ('2', 'Queue', 'Queue', '2024-08-13 15:13:01', NULL);
+INSERT INTO `pipeline_status` (`id`, `name`, `value`, `create_time`, `update_time`) VALUES ('3', 'Process', 'Process', '2024-08-13 15:13:01', NULL);
+INSERT INTO `pipeline_status` (`id`, `name`, `value`, `create_time`, `update_time`) VALUES ('4', 'Success', 'Success', '2024-08-13 15:13:01', NULL);
+INSERT INTO `pipeline_status` (`id`, `name`, `value`, `create_time`, `update_time`) VALUES ('5', 'Failed', 'Failed', '2024-08-13 15:13:01', NULL);
+INSERT INTO `pipeline_status` (`id`, `name`, `value`, `create_time`, `update_time`) VALUES ('6', 'Stop', 'Stop', '2024-08-13 15:13:01', NULL);
+COMMIT;
 
 -- ----------------------------
 -- Table structure for pipeline_step
@@ -111,11 +195,14 @@ CREATE TABLE `pipeline_step_component` (
   `id` varchar(255) NOT NULL,
   `step_id` varchar(255) DEFAULT NULL,
   `prop` varchar(255) DEFAULT NULL,
+  `label` varchar(255) DEFAULT NULL,
   `value` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
   `create_time` varchar(255) DEFAULT NULL,
   `update_time` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 -- ----------------------------
 -- Table structure for pipeline_tag
 -- ----------------------------
@@ -151,6 +238,7 @@ DROP TABLE IF EXISTS `pipeline_variable`;
 CREATE TABLE `pipeline_variable` (
   `id` varchar(255) DEFAULT NULL,
   `pipeline_id` varchar(255) DEFAULT NULL,
+  `order` int DEFAULT NULL COMMENT '顺序',
   `name` varchar(255) DEFAULT NULL COMMENT '变量名',
   `genre` varchar(255) DEFAULT NULL COMMENT '变量类型',
   `value` varchar(255) DEFAULT NULL COMMENT '值',
@@ -160,6 +248,7 @@ CREATE TABLE `pipeline_variable` (
   `create_time` varchar(255) DEFAULT NULL,
   `update_time` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 
 -- ----------------------------
 -- Table structure for server
@@ -177,6 +266,5 @@ CREATE TABLE `server` (
   `update_time` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
 
 SET FOREIGN_KEY_CHECKS = 1;
